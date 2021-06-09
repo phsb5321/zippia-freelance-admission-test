8f0f514c06213f9b9159d6ea2c7f164e4e25397a
phsb5321 <phsb5321@gmail.com>
2021-06-09 14:00:42 -0300

    feature(minimal-shippin-viable): everything up and running

--- out/_next/static/x1DiCde5FoCV4GBRUdLAT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/feae718bacaeb373132b.css","static/chunks/pages/index-693375211e57cb6c1cd1.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
